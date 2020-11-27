559b79a47708dc4a70ab4017061ac6231db442bc
EmileRolley <erolley@tutamail.com>
2020-11-27 13:17:43 +0100

    [CALC] - Add dependency between lib and calc.

--- paleo-calc/src/main/java/paleo/calc/Calculator.java
+++ paleo-calc/src/main/java/paleo/calc/Calculator.java
@@ -1,12 +1,14 @@
 package paleo.calc;
 
+import paleo.lib.parser.Parser;
+
 /**
 * Calculator
 */
 public class Calculator {
 
 	public static void main (String[] args) {
+		Parser parser = new Parser();
 		System.out.println("Calculator comming soon...");
 	}
-
 }
