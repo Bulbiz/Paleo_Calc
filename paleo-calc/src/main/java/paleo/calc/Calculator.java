1c86474c9a85bd4d22c2bfbc41bba664f323b570
EmileRolley <erolley@tutamail.com>
2020-11-27 13:10:16 +0100

    [CALC] - Add paleo-calc/

---
+++ paleo-calc/src/main/java/paleo/calc/Calculator.java
@@ -0,0 +1,12 @@
+package paleo.calc;
+
+/**
+* Calculator
+*/
+public class Calculator {
+
+	public static void main (String[] args) {
+		System.out.println("Calculator comming soon...");
+	}
+
+}
---
+++ paleo-calc/src/test/java/paleo/calc/CalculatorTest.java
@@ -0,0 +1,16 @@
+package paleo.calc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+* TestCalculator
+*/
+public class CalculatorTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
