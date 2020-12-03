45b024909bce2fb5e90307e9fd75e32476fe0f60
EmileRolley <erolley@tutamail.com>
2020-12-03 18:04:39 +0100

    [INTERPRETER_TEST] - Add tests for simple expression with only one operation and two integer operand.

--- paleo-lib/src/test/java/paleo/lib/interpreter/InterpreterTest.java
+++ paleo-lib/src/test/java/paleo/lib/interpreter/InterpreterTest.java
@@ -28,4 +28,28 @@ public class InterpreterTest {
         );
     }
 
+    @Test
+    public void simpleIntegerSub() {
+        assertEquals(
+            new IntegerOperandToken(-2),
+            new Interpreter(new Parser("3 - 5").parse()).evaluate()
+        );
+    }
+
+    @Test
+    public void simpleIntegerDiv() {
+        assertEquals(
+            new IntegerOperandToken(0),
+            new Interpreter(new Parser("3 / 5").parse()).evaluate()
+        );
+    }
+
+    @Test
+    public void simpleIntegerMult() {
+        assertEquals(
+            new IntegerOperandToken(15),
+            new Interpreter(new Parser("3 * 5").parse()).evaluate()
+        );
+    }
+
 }
