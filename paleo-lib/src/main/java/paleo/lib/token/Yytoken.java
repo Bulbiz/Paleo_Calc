21b7cf3a667549701d04a21d0acd8b7578b38ca1
EmileRolley <erolley@tutamail.com>
2020-12-04 17:00:29 +0100

    [TOKEN] - Updates Yytoken.java comments

--- paleo-lib/src/main/java/paleo/lib/token/Yytoken.java
+++ paleo-lib/src/main/java/paleo/lib/token/Yytoken.java
@@ -1,5 +1,10 @@
 package paleo.lib.token;
 
+/**
+ * {@link FunctionalInterface} neeeded to tokenize expressions with JFlex.
+ * @see JFLexer
+ */
+@FunctionalInterface
 public interface Yytoken {
     boolean isAnOperandToken();
 }
