682f1f3b4b2a1a5e62a223eee12ae0e1abf4fd32
Pholasa Remy <pholasa2@outlook.fr>
2020-12-09 22:53:20 +0100

    Adaptation du OperationDictionnary for the new class

--- paleo-lib/src/main/java/paleo/lib/interpreter/OperationDictionary.java
+++ paleo-lib/src/main/java/paleo/lib/interpreter/OperationDictionary.java
@@ -18,10 +18,7 @@ public final class OperationDictionary {
 	/**
 	 * Stores all {@link OperationEvaluator}.
 	 */
-	private static HashMap<OperationToken,
-							HashMap<Class <? extends OperandToken>,
-									HashMap<Class <? extends OperandToken>,
-										 OperationEvaluator>>>
+	private static HashMap<OperationToken,OperationDictionnaryEntry>
 							operationMap = new HashMap<>();
 
 	/**
@@ -38,14 +35,21 @@ public final class OperationDictionary {
 			OperationEvaluator opEvaluator,
 			Class<? extends OperandToken> ... signature)
 	{
-		if (null == operationMap.get(operation)) {
-			operationMap.put(operation, new HashMap<>());
-		}
-		if (null == operationMap.get(operation).get(signature[0])) {
-			operationMap.get(operation).put(signature[0], new HashMap<>());
+		if (!operationMap.containsKey(operation)) {
+			operationMap.put(operation, new OperationDictionnaryEntry ());
 		}
 
-		operationMap.get(operation).get(signature[0]).put(signature[1], opEvaluator);
+		/* A CONTINUER + FAIRE LE EVALUATOR FINDER */
+		OperationDictionnaryEntry operatorFinder = operationMap.get(operation);
+
+		for(int i=0 ;  i< signature.length; i++){
+			if (!operatorFinder.containsKey(signature[i])) {
+				operatorFinder.put(signature[i]);
+			}
+			operatorFinder = operatorFinder.next(signature[i]);
+		}
+
+		operatorFinder.setEvaluator(opEvaluator);
 	}
 
 	/**
@@ -76,7 +80,7 @@ public final class OperationDictionary {
 					+ "'"
 			);
 		}
-		if (!operationMap.get(operation).get(op1).containsKey(op2)) {
+		if (!operationMap.get(operation).next(op1).containsKey(op2)) {
 			throw new IllegalArgumentException(
 					operation.toString() + " "
 					+ "unsupported operation for the operand '"
@@ -86,6 +90,6 @@ public final class OperationDictionary {
 					+ "'"
 			);
 		}
-		return operationMap.get(operation).get(op1).get(op2);
+		return operationMap.get(operation).next(op1).next(op2).getEvaluator().get();
 	}
 }
