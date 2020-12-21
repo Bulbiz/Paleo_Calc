b90732e65fade9751af784aec5a49761fed60fc9
EmileRolley <erolley@tutamail.com>
2020-12-21 10:54:08 +0100

    [OPERAND] - Factorized isAnOperandToken()

--- paleo-lib/src/main/java/paleo/lib/token/OperandToken.java
+++ paleo-lib/src/main/java/paleo/lib/token/OperandToken.java
@@ -3,4 +3,14 @@ package paleo.lib.token;
 /**
  * Models a generic operand type.
  */
-public interface OperandToken extends Yytoken {}
+public interface OperandToken extends Yytoken {
+	/**
+	 * Allows to distinguish the instances of {@link OperandToken}
+	 * and {@link OperationToken}.
+	 *
+	 * @return true.
+	 */
+	default boolean isAnOperandToken() {
+		return true;
+	}
+}
