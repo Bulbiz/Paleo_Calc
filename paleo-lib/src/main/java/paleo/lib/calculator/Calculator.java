0fe7c3a665731770b982b24f15d9bb76b914d5fc
EmileRolley <erolley@tutamail.com>
2021-01-06 19:36:15 +0100

    [CALCULATOR] - Complete documentation for the paleo.lib.calculator subpackage.

--- paleo-lib/src/main/java/paleo/lib/calculator/Calculator.java
+++ paleo-lib/src/main/java/paleo/lib/calculator/Calculator.java
@@ -4,8 +4,14 @@ import java.util.Optional;
 import paleo.lib.token.operand.OperandToken;
 
 /**
- * Calculator
+ * {@link Calculator} provides an interface in order to "calculate a line", means :
+ *
+ *   1. Parse the line.
+ *   2. Evaluate the parsed line.
+ *   3. Return the resulting value if the line corresponds to a valid
+ *   mathematical expression.
  */
+@FunctionalInterface
 public interface Calculator {
 	public Optional<OperandToken> calculate(final String line);
 }
--- paleo-lib/src/main/java/paleo/lib/calculator/HistCalculator.java
+++ paleo-lib/src/main/java/paleo/lib/calculator/HistCalculator.java
@@ -8,22 +8,37 @@ import paleo.lib.parser.Parser;
 import paleo.lib.token.Yytoken;
 import paleo.lib.token.operand.OperandToken;
 
+/**
+ * {@link HistCalculator} implements {@link Calculator} by adding an historic
+ * management through the {@link HistoricManager} instance.
+ */
 public class HistCalculator implements Calculator {
 
-	private final Interpreter.Factory interpreterFactory;
-	private final Parser parser;
-	private final HistoricManager historicManager;
+	private final Interpreter.Factory interpreterFactory; ///< Allows to creates an {@link Interpreter} instance from an expression.
+	private final Parser parser; ///< {@link Parser} instance used to parse the line.
+	private final HistoricManager historicManager; ///< Is the historic manager.
 
+	/**
+	 *  {@link HistCalculator} constructor.
+	 */
 	public HistCalculator(
-		Interpreter.Factory interpreterFactory,
-		Parser parser,
-		HistoricManager historicManager
+		final Interpreter.Factory interpreterFactory,
+		final Parser parser,
+		final HistoricManager historicManager
 	) {
 		this.interpreterFactory = interpreterFactory;
 		this.parser = parser;
 		this.historicManager = historicManager;
 	}
 
+	/**
+	 * Calculates the given line (@see Calculator).
+	 *
+	 * @line is the line to be calculate.
+	 * @return an {@link Optional}, if the line could be evaluate like a valid
+	 * mathematical expression return the corresponding value, otherwise, an
+	 * empty optional.
+	 */
 	public Optional<OperandToken> calculate(final String line) {
 		Optional<OperandToken> optionalOp;
 
@@ -41,7 +56,7 @@ public class HistCalculator implements Calculator {
 
 	private Optional<OperandToken> evaluate(final String expr) {
 		Interpreter interpreter;
-		Optional<Queue<Yytoken>> tokenExpression = this.parser.parse(expr);
+		final Optional<Queue<Yytoken>> tokenExpression = this.parser.parse(expr);
 
 		if (tokenExpression.isPresent()) {
 			try {
@@ -50,7 +65,7 @@ public class HistCalculator implements Calculator {
 							historicManager.substitute(tokenExpression.get()).get()
 						);
 				return Optional.of(interpreter.evaluate());
-			} catch (Exception e) {
+			} catch (final Exception e) {
 				//TODO: handle exception
 				System.err.println(e.getMessage());
 			}
